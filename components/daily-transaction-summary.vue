232b3865a8e9ef993134340ba50d27d4fb0ab27f
hoang227 <hoangg227@gmail.com>
2024-04-10 21:14:31 +0800

    implement summary for daily transactions

---
+++ components/daily-transaction-summary.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="grid grid-cols-2 py-4 border-b border-gray-200 dark:border-gray-800 text-gray-500 dark:text-gray-400">
+    <div class="flex items-center justify-between">
+      {{ date }}
+    </div>
+    <div class="flex items-center justify-end mr-10">
+      {{ currency }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  date: {
+    type: String,
+    required: true
+  },
+  transactions: {
+    type: Array as () => Transaction[],
+    required: true
+  }
+})
+
+const sum = computed(() => {
+  let sum = 0
+  for (const transaction of props.transactions) {
+    if (transaction.type === 'income') {
+      sum += transaction.amount ?? 0
+    } else {
+      sum -= transaction.amount ?? 0
+    }
+  }
+  return sum
+})
+
+const { currency } = useCurrency(sum)
+</script>
