9e50ff2f24e9f79535639a5daaf844db6f2a5eac
hoang227 <hoangg227@gmail.com>
2024-04-18 14:01:53 +0800

    add login and signup page

---
+++ pages/login.vue
@@ -0,0 +1,151 @@
+<template>
+  <UTabs :items="items" class="w-full">
+    <template #signin="{ item }">
+      <UForm ref="signinForm" :state="signinState" :schema="signinSchema" @submit="handleSignIn">
+        <UCard>
+          <template #header>
+            {{ item.label }} now !
+          </template>
+
+          <div class="space-y-3">
+            <UFormGroup label="email" name="email">
+              <UInput v-model="signinState.email" placeholder="email@example.com" />
+            </UFormGroup>
+            <UFormGroup label="password" name="password">
+              <UInput v-model="signinState.password" type="password" placeholder="password" />
+            </UFormGroup>
+          </div>
+
+          <template #footer>
+            <UButton type="submit" color="black" :loading="isLoading">
+              {{ item.label }}
+            </UButton>
+          </template>
+        </UCard>
+      </UForm>
+    </template>
+
+    <template #signup="{ item }">
+      <UForm ref="signupForm" :state="signupState" :schema="signupSchema" @submit="handleSignUp">
+        <UCard>
+          <template #header>
+            {{ item.label }} now !
+          </template>
+
+          <div class="space-y-3">
+            <UFormGroup label="email" name="email" required>
+              <UInput v-model="signupState.email" placeholder="email@example.com" />
+            </UFormGroup>
+            <UFormGroup label="username" name="username" required>
+              <UInput v-model="signupState.username" placeholder="username" />
+            </UFormGroup>
+            <UFormGroup label="password" name="password" required>
+              <UInput
+                v-model="signupState.password"
+                placeholder="password"
+                type="password"
+              />
+            </UFormGroup>
+            <p>
+              • password must be at least 8 characters
+            </p>
+            <p>
+              • password must be at most 20 characters
+            </p>
+            <p>
+              • password must contain at least one uppercase letter, one lowercase letter and one digit.
+            </p>
+          </div>
+
+          <template #footer>
+            <UButton type="submit" color="black" :loading="isLoading">
+              {{ item.label }}
+            </UButton>
+          </template>
+        </UCard>
+      </UForm>
+    </template>
+  </UTabs>
+</template>
+
+<script setup lang="ts">
+import { z } from 'zod'
+
+const supabase = useSupabaseClient()
+const { toastSuccess, toastError } = useAppToast()
+const isLoading = ref(false)
+const signinForm = ref()
+const signupForm = ref()
+
+const items = [{
+  slot: 'signin',
+  label: 'sign in'
+}, {
+  slot: 'signup',
+  label: 'sign up'
+}]
+
+const signinSchema = z.object({
+  email: z.string().email({ message: 'invalid email address' }).optional(),
+  password: z.string().optional()
+})
+
+const signupSchema = z.object({
+  email: z.string().email({ message: 'invalid email address' }),
+  username: z.string(),
+  password: z.string().min(8, { message: 'password must be at least 8 characters' }).max(20, { message: 'password must be at most 20 characters' }).regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,20}$/, { message: 'password must contain at least one uppercase letter, one lowercase letter and one digit.' })
+})
+
+const initialSigninState = {
+  email: '',
+  password: ''
+}
+
+const initialSignupState = {
+  email: '',
+  username: '',
+  password: ''
+}
+
+const signinState = ref({ ...initialSigninState })
+const signupState = ref({ ...initialSignupState })
+
+const handleSignIn = async () => {
+  isLoading.value = true
+
+  try {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: signinState.value.email,
+      password: signinState.value.password
+    })
+
+    console.log(data)
+
+    if (!error) {
+      toastSuccess({
+        title: 'signed in successfully'
+      })
+    }
+
+    throw error
+  } catch (error) {
+    Object.assign(signinState.value, initialSigninState)
+    signinForm.value.clear()
+    toastError({
+      title: 'wrong email or password'
+    })
+  } finally {
+    isLoading.value = false
+  }
+}
+
+const handleSignUp = () => {
+  console.log('hello')
+}
+</script>
+
+<style scoped>
+p {
+  @apply text-xs text-gray-400
+}
+</style>
