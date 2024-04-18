2911b7dd4e1f37a12905a623d6de9b6a5d0f698c
hoang227 <hoangg227@gmail.com>
2024-04-18 14:23:16 +0800

    add logout button

--- components/app-header.vue
+++ components/app-header.vue
@@ -3,8 +3,36 @@
     <NuxtLink to="/" class="text-xl">
       coin.sense
     </NuxtLink>
-    <div>
+    <div class="flex items-center justify-between space-x-2">
+      <UButton class="" label="logout" color="white" variant="ghost" @click="handleLogout" />
       <UAvatar src="https://avatars.githubusercontent.com/u/739984?v=4" alt="Avatar" />
     </div>
   </header>
 </template>
+
+<script setup lang="ts">
+
+const supabase = useSupabaseClient()
+const { toastSuccess, toastError } = useAppToast()
+
+const handleLogout = async () => {
+  try {
+    const { error } = await supabase.auth.signOut()
+
+    if (!error) {
+      toastSuccess({
+        title: 'successfully logged out'
+      })
+    } else {
+      throw error
+    }
+  } catch (error) {
+    toastError({
+      title: 'failed to logged out'
+    })
+  }
+
+  return navigateTo('/login')
+}
+
+</script>
